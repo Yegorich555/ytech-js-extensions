96db71933db6799546f27453f1ef707fd65a020d
Yegor <Yegorich_555@mail.ru>
2018-10-26 16:03:19 +0300

    Extensions. Split to functions

--- index.js
+++ index.js
@@ -5,8 +5,10 @@
 */
 /* global define */
 
-require('./lib/array.js');
-require('./lib/date.js');
-require('./lib/math.js');
-require('./lib/string.js');
-require('./lib/object/index.js');
+require('./lib/array/index.js');
+require('./lib/date/index.js');
+require('./lib/math/index.js');
+require('./lib/string/index.js');
+module.exports = {
+    equalOptions: require('./lib/object/index.js')
+}
