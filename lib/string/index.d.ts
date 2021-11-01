15a628937197036185adf270d01f985573197ba0
Yegor G <yegorich_555@mail.ru>
2021-11-01 16:56:34 +0300

    TS. Fix "import doesn't assign to global object"

--- lib/string/index.d.ts
+++ lib/string/index.d.ts
@@ -1,29 +1,33 @@
-interface String {
-  /**
-   * Compare string with ignore case
-   *
-   * @param {String} v
-   * @return {Boolean} true if equal
-   */
-  equal: typeof import("./equal").default;
-  /**
-   * Set UpperCase for the first character
-   *
-   * @return {String} new string if string is not null otherwise same string
-   */
-  toUpperCaseFirst: typeof import("./toUpperCaseFirst").default;
-  /**
-   * Set LowerCase for the first character
-   *
-   * @return {String} new string if string is not null otherwise same string
-   */
-  toLowerCaseFirst: typeof import("./toLowerCaseFirst").default;
-  /**
-   * Split string by spaces and join to camelCase: 'My camel Case string' to 'myCamelCaseString'
-   */
-  toCamelCase: typeof import("./toCamelCase").default;
-  /**
-   * Transform string from camelCase: 'myCamelCaseString' to 'My Camel Case String'
-   */
-  fromCamelCase: typeof import("./fromCamelCase").default;
+export {};
+
+declare global {
+  interface String {
+    /**
+     * Compare string with ignore case
+     *
+     * @param {String} v
+     * @return {Boolean} true if equal
+     */
+    equal: typeof import("./equal").default;
+    /**
+     * Set UpperCase for the first character
+     *
+     * @return {String} new string if string is not null otherwise same string
+     */
+    toUpperCaseFirst: typeof import("./toUpperCaseFirst").default;
+    /**
+     * Set LowerCase for the first character
+     *
+     * @return {String} new string if string is not null otherwise same string
+     */
+    toLowerCaseFirst: typeof import("./toLowerCaseFirst").default;
+    /**
+     * Split string by spaces and join to camelCase: 'My camel Case string' to 'myCamelCaseString'
+     */
+    toCamelCase: typeof import("./toCamelCase").default;
+    /**
+     * Transform string from camelCase: 'myCamelCaseString' to 'My Camel Case String'
+     */
+    fromCamelCase: typeof import("./fromCamelCase").default;
+  }
 }
